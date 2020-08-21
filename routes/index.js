f158b0b331e8be97d2dfcb15155b39cdd2234bce
David Viramontes <dviramontes29@yahoo.com>
2020-08-21 09:36:34 -0700

    testing

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
 router.use('/api', require('./apiRoutes.js'))
-router.use('/', require('./viewRoutes.js'))
+//router.use('/', require('./viewRoutes.js'))
 
 module.exports = router
