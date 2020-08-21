c8af925841190df84513573c5dfccb84dc5ed98f
David Viramontes <dviramontes29@yahoo.com>
2020-08-21 09:41:48 -0700

    router.get

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 const router = require('express').Router()
 
 router.use('/api', require('./apiRoutes.js'))
-router.use('/', require('./viewRoutes.js'))
+
+router.get('/notes', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'public', 'notes.html'))
+  })
+  
+  router.get('/*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'public', 'index.html'))
+  })
 
 module.exports = router
